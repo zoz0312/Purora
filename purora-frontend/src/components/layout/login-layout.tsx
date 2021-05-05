906b4c203a9c40f0056d1fbc26742397d76bd19e
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-05-05 19:42:54 +0900

    [feat] - Add header

--- purora-frontend/src/components/layout/login-layout.tsx
+++ purora-frontend/src/components/layout/login-layout.tsx
@@ -21,7 +21,7 @@ export interface NavigationProps {
 
 const navigation: NavigationMenu[] = [
   {
-    name: 'Dashboard',
+    name: 'Home',
     host: '/',
   },
 ];
@@ -68,8 +68,12 @@ const LoginLayout: React.FC<LoginLayoutProps> = (
           </>
         )}
       </Disclosure>
-      <header>
-        Header
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">
+            {navigation.find(({ host, name }) => host === pathname)?.name}
+          </h1>
+        </div>
       </header>
       { children }
     </div>
