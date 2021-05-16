28fdee8480ce97994e36dce0ba39c90bc48554da
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-05-16 15:15:53 +0900

    [modify] - 404 page fix

--- purora-frontend/src/components/NotFound.tsx
+++ purora-frontend/src/components/NotFound.tsx
@@ -1,16 +1,12 @@
+import {Link} from "react-router-dom";
 
 const NotFound: React.FC = () => {
   return (
-    <>
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-      Not Found<br />
-    </>
+    <div className={'max-w-xl mx-auto p-5'}>
+      <h1 className={'text-gray-800 text-4xl font-bold'}>Page Not Found</h1>
+      <h2 className={'text-xl text-gray-700 mt-5'}>페이지를 찾을 수 없습니다.</h2>
+      <Link to={'/'} className={'underline text-blue-700'}>메인페이지로 이동</Link>
+    </div>
   )
 }
 
